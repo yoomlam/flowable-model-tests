88d3423a70620f89e1dedb116a1f417c4d1fb11d
Yoom Lam <yoom@navapbc.com>
2023-09-19 17:41:56 -0500

    IEE test: rename IDs for readability

--- src/test/kotlin/testing/flowable/flex/IntegratedEnrollmentAndEligibilityTest.kt
+++ src/test/kotlin/testing/flowable/flex/IntegratedEnrollmentAndEligibilityTest.kt
@@ -1,7 +1,7 @@
 package testing.flowable.flex
 
 import com.github.tomakehurst.wiremock.client.WireMock
-import com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension
 import org.assertj.core.api.Assertions.assertThat
 import org.flowable.engine.HistoryService
@@ -18,7 +18,6 @@ import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import testing.flowable.FlowableConfiguration
 import testing.flowable.simple.SimpleConfiguration
-import kotlin.test.BeforeTest
 
 @ExtendWith(FlowableSpringExtension::class)
 @ExtendWith(SpringExtension::class)
@@ -35,40 +34,45 @@ class IntegratedEnrollmentAndEligibilityTest {
     class MySpecificConfig
 
     companion object {
+        const val applicationId = 20230919
+
         @RegisterExtension
         @JvmStatic
         val mockApi: WireMockExtension = WireMockExtension.newInstance()
             .failOnUnmatchedRequests(true)
-            .options(wireMockConfig().port(3000))
+            .options(WireMockConfiguration.wireMockConfig().port(3000))
             .build()
-
-        const val applicationId = 1234
     }
 
-    @BeforeTest
-    fun setup() {
+    private fun stubResponses(
+        healthcareEligibilityResult: String = "",
+        foodEligibilityResult: String = ""
+    ) {
         mockApi.stubFor(
             WireMock.post("/api/applications/$applicationId/notifications")
                 .willReturn(WireMock.badRequest())
         )
-        mockApi.stubFor(
-            WireMock.post("/api/eligibility")
-                .willReturn(
-                    WireMock.aResponse()
-                        .withBody("{ \"eligible\": true}")
-                )
-        )
+        // Eligibility table: https://github.com/navapbc/benefit-delivery-systems/blob/cdb23eb1f02a0f367cfc864ff89505dfce36e217/portal/src/pages/api/eligibility/healthcare.ts#L20
         mockApi.stubFor(
             WireMock.post("/api/eligibility/healthcare")
                 .willReturn(
                     WireMock.aResponse()
-                        .withBody("{ \"eligible\": true}")
+                        // EligiblityResult possible values: Adults, PregnantWomen, ChildrenUnder19, NotEligible
+                        // https://github.com/navapbc/benefit-delivery-systems/blob/cdb23eb1f02a0f367cfc864ff89505dfce36e217/portal/src/pages/api/eligibility/healthcare.ts#L10
+                        .withBody("{ \"eligibility_result\": \"$healthcareEligibilityResult\" }")
                 )
         )
         mockApi.stubFor(
-            WireMock.post("/api/notifications")
-                .willReturn(WireMock.badRequest())
+            WireMock.post("/api/eligibility/food")
+                .willReturn(
+                    WireMock.aResponse()
+                        // EligiblityResult possible values: Eligible, NotEligible
+                        // https://github.com/navapbc/benefit-delivery-systems/blob/cdb23eb1f02a0f367cfc864ff89505dfce36e217/portal/src/pages/api/eligibility/food.ts#L10
+                        .withBody("{ \"eligibility_result\": \"$foodEligibilityResult\" }")
+                )
         )
+        // (eligibility_result == 'PregnantWomen' || eligibility_result == 'ChildrenUnder19') ?
+        // 'mixed' : 'approved' is the payload to /api/applications/$applicationId/notifications
     }
 
     @Autowired
@@ -80,60 +84,165 @@ class IntegratedEnrollmentAndEligibilityTest {
     @Autowired
     lateinit var historyService: HistoryService
 
+    // Variables going into the process from the startEvent
+    // https://github.com/navapbc/benefit-delivery-systems/blob/cdb23eb1f02a0f367cfc864ff89505dfce36e217/portal/src/pages/api/applications/index.ts#L40
+    private val defaultProcessVariables = mapOf(
+        // applicationId is used in /api/applications/$applicationId/notifications
+        "applicationId" to applicationId,
+        "benefitProgramName" to "healthcare",
+        // applicationIncome and householdSize are used in the payload to /api/eligibility/*
+        "applicationIncome" to 3500 * 100, // in cents
+        "householdSize" to 2
+        // Other application fields are saved to the DB for use by other api calls -- see
+        // https://github.com/navapbc/benefit-delivery-systems/blob/cdb23eb1f02a0f367cfc864ff89505dfce36e217/portal/prisma/schema.prisma#L30
+        // The DB is queried for api/.../notifications calls
+        // https://github.com/navapbc/benefit-delivery-systems/blob/cdb23eb1f02a0f367cfc864ff89505dfce36e217/portal/src/pages/api/applications/%5Bapplication_id%5D/notifications.ts#L40
+        // TODO: applicationIncome and householdSize are being passed as process variables via the workflow to be
+        //   passed as part the payload to only api/eligibility/* calls.
+        //   When should this info be pulled from the DB, rather than as process variables?
+    )
+
+    // Convenience method to create correct-typed map of output variable values from tasks, i.e., UserTasks
+    private inline fun taskOutputMap(vararg pairs: Pair<String, Any>) = mapOf(*pairs)
+
+    // Tip: Go to https://bpmn-io.github.io/bpmn-js-token-simulation/modeler.html?pp=1 and open the bpmn file.
     @Test
     @Deployment(resources = ["processes/integratedEnrollmentAndEligibility_LOCALHOST.bpmn"])
-    fun simpleProcessTest() {
-        // Create and start process instance
-        val processVariables: Map<String, Any> = mapOf(
-            "applicationId" to applicationId,
-            "applicationIncome" to 350000,
-            "householdSize" to 2
-        )
-        runtimeService.startProcessInstanceByKey("integratedEnrollmentAndEligibilityDemo", processVariables)
-
-        // Process instance is created and is waiting for completion
-        assertThat(runtimeService.createProcessInstanceQuery().list()).isNotEmpty()
-
-        // Get the single active task in the process
-        val makeDeterminationTask = taskService.createTaskQuery().taskName("Makes determination").singleResult()
-        taskService.complete(makeDeterminationTask.id)
-
-//        val verifyIncomeTask = taskService.createTaskQuery().taskName("Verifies income").singleResult()
-
-//        // Verify that a method on the TestService 'someService' is called (for serviceTask1)
-//        verify(someService).logMessage("Hello")
-//        // Verify that a method on the SomeApiClient is called (for serviceTask2)
-//        verify(apiClient2).callApiEndpoint()
-
+    fun defaultPath() {
+        stubResponses()
         val expectedActivities = arrayOf(
-            "applicationSubmitted",
-            "bpmnSequenceFlow_2", "splitOnProgramType",
-            "bpmnSequenceFlow_15", "bpmnTask_8",
-            "bpmnSequenceFlow_18", "bpmnGateway_17",
-            "bpmnSequenceFlow_19", "makesDetermination",
-            "bpmnSequenceFlow_3", "sendApprovalNotification",
-            "bpmnSequenceFlow_12", "applicationProcessed"
+            "applicationSubmitted", "withApplication", "programTypeGW",
+            "whenHealthcareProgram", "checkHealthcareEligibility", "withHealthcareApiResponse", "healthcareResultGW",
+            // chooses checkHealthcareEligibility path b/c flow healthcareProgram has no 'conditionExpression' defined
+            // checkHealthcareEligibility has field (responseVariableName = eligibilityResponse)
+            // "healthcareResultGW" default="whenHealthcareEligible"
+            "whenHealthcareEligible", "makeDetermination", "determinationMade", "sendApprovalNotification", "approvalSent", "applicationProcessed"
+            // userTask id="makeDetermination" name="Makes determination" flowable:dueDate="P5D"
+            // dueDate format: https://documentation.flowable.com/latest/model/forms/reference/date/index.html#formats
         )
-        assertCompletion(userTasksRan = 1, expectedActivities)
+        val userTasks = linkedMapOf("makeDetermination" to taskOutputMap())
+        runToCompletion(defaultProcessVariables, expectedActivities, userTasks)
     }
 
-    private fun assertCompletion(userTasksRan: Int, expectedActivities: Array<String>) {
+    @Test
+    @Deployment(resources = ["processes/integratedEnrollmentAndEligibility_LOCALHOST.bpmn"])
+    fun eligibleForHealthcare() {
+        defaultPath()
+    }
+
+    @Test
+    @Deployment(resources = ["processes/integratedEnrollmentAndEligibility_LOCALHOST.bpmn"])
+    fun notEligibleForHealthcare() {
+        stubResponses(healthcareEligibilityResult = "NotEligible")
+        val processVariables = defaultProcessVariables
+        val expectedActivities = arrayOf(
+            "applicationSubmitted", "withApplication", "programTypeGW",
+            "whenHealthcareProgram", "checkHealthcareEligibility", "withHealthcareApiResponse", "healthcareResultGW",
+            "whenNotHealthcareEligible", "sendDenialNotification", "denialSent", "applicationProcessed"
+        )
+        runToCompletion(processVariables, expectedActivities)
+    }
+
+    @Test
+    @Deployment(resources = ["processes/integratedEnrollmentAndEligibility_LOCALHOST.bpmn"])
+    fun eligibleForEnergy() {
+        stubResponses()
+        val processVariables = defaultProcessVariables + mapOf("benefitProgramName" to "energy")
+        val expectedActivities = arrayOf(
+            "applicationSubmitted", "withApplication", "programTypeGW",
+            "whenEnergyProgram", "makeDetermination", "determinationMade", "sendApprovalNotification", "approvalSent", "applicationProcessed"
+        )
+        val userTasks = linkedMapOf("makeDetermination" to taskOutputMap())
+        runToCompletion(processVariables, expectedActivities, userTasks)
+    }
+
+    @Test
+    @Deployment(resources = ["processes/integratedEnrollmentAndEligibility_LOCALHOST.bpmn"])
+    fun notEligibleForEnergy() {
+        // TODO: No such path when benefitProgramName = energy
+    }
+
+    @Test
+    @Deployment(resources = ["processes/integratedEnrollmentAndEligibility_LOCALHOST.bpmn"])
+    fun eligibleForFoodAndIncomeVerified() {
+        stubResponses(foodEligibilityResult = "Eligible")
+        val processVariables = defaultProcessVariables + mapOf("benefitProgramName" to "food")
+        val expectedActivities = arrayOf(
+            "applicationSubmitted",
+            "withApplication", "programTypeGW",
+            "whenFoodProgram", "checkFoodEligibility", "withFoodApiResponse", "foodResultGW",
+            "whenEligibleFood", "verifyIncome", "withIncomeVerification", "incomeVerificationGW",
+            "whenIncomeVerified", "makeDetermination", "determinationMade", "sendApprovalNotification", "approvalSent", "applicationProcessed"
+        )
+        val userTasks = linkedMapOf(
+            "verifyIncome" to taskOutputMap("sufficient_proof_of_income_response" to "is_sufficient"),
+            "makeDetermination" to taskOutputMap()
+        )
+        runToCompletion(processVariables, expectedActivities, userTasks)
+    }
+
+    @Test
+    @Deployment(resources = ["processes/integratedEnrollmentAndEligibility_LOCALHOST.bpmn"])
+    fun eligibleForFoodButIncomeNotVerified() {
+        stubResponses(foodEligibilityResult = "Eligible")
+        val processVariables = defaultProcessVariables + mapOf("benefitProgramName" to "food")
+        val expectedActivities = arrayOf(
+            "applicationSubmitted", "withApplication", "programTypeGW",
+            "whenFoodProgram", "checkFoodEligibility", "withFoodApiResponse", "foodResultGW",
+            "whenEligibleFood", "verifyIncome", "withIncomeVerification", "incomeVerificationGW",
+            "whenNotIncomeVerified", "sendDenialNotification", "denialSent", "applicationProcessed"
+        )
+        // https://github.com/navapbc/benefit-delivery-systems/blob/cdb23eb1f02a0f367cfc864ff89505dfce36e217/portal/src/components/staff/features/task-actions/VerifyIncome.tsx#L64C20-L64C28
+        val insufficientResponse = arrayOf("need_rfi", "need_integrity_review").random()
+        val userTasks = linkedMapOf(
+            "verifyIncome" to taskOutputMap("sufficient_proof_of_income_response" to insufficientResponse)
+        )
+        runToCompletion(processVariables, expectedActivities, userTasks)
+    }
+
+    @Test
+    @Deployment(resources = ["processes/integratedEnrollmentAndEligibility_LOCALHOST.bpmn"])
+    fun notEligibleForFood() {
+        stubResponses(foodEligibilityResult = "NotEligible")
+        val processVariables = defaultProcessVariables + mapOf("benefitProgramName" to "food")
+        val expectedActivities = arrayOf(
+            "applicationSubmitted", "withApplication", "programTypeGW",
+            "whenFoodProgram", "checkFoodEligibility", "withFoodApiResponse", "foodResultGW",
+            "whenNotEligibleFood", "sendDenialNotification", "denialSent", "applicationProcessed"
+        )
+        runToCompletion(processVariables, expectedActivities)
+    }
+
+    private fun completeTask(taskKey: String, taskOutputVariables: Map<String, Any>) {
+        val task = taskService.createTaskQuery().taskDefinitionKey(taskKey).singleResult()
+        taskService.complete(task.id, taskOutputVariables)
+    }
+
+    private fun runToCompletion(
+        processVariables: Map<String, Any>,
+        expectedActivities: Array<String>,
+        userTasks: LinkedHashMap<String, Map<String, Any>> = LinkedHashMap()
+    ) {
+        // Start process with processVariables
+        runtimeService.startProcessInstanceByKey("integratedEnrollmentAndEligibilityDemo", processVariables)
+
+        userTasks.forEach { completeTask(it.key, it.value) }
+
         // All tasks are complete, so there are no active tasks
         assertThat(taskService.createTaskQuery().list()).isEmpty()
         // Process instance is now completed
         assertThat(runtimeService.createProcessInstanceQuery().list()).isEmpty()
-
         // Check history
         assertThat(historyService.createHistoricProcessInstanceQuery().count()).isEqualTo(1)
-
-        // Apparently this only returns UserTasks?
-        val tasks = historyService.createHistoricTaskInstanceQuery().orderByHistoricTaskInstanceStartTime().asc().list()
-        println("tasks: $tasks")
-        assertThat(tasks.size).isEqualTo(userTasksRan)
 
         // Activities have tasks and sequenceFlows that were executed
         val activities = historyService.createHistoricActivityInstanceQuery().orderByHistoricActivityInstanceStartTime().asc().list()
         val activityIds = activities.map { it.activityId }
         assertThat(activityIds).containsSequence(*expectedActivities)
+
+        // Check userTasks that were ran
+        val userTasksRan = historyService.createHistoricTaskInstanceQuery().orderByHistoricTaskInstanceStartTime().asc().list()
+        val userTaskKeys = userTasksRan.map { it.taskDefinitionKey }
+        assertThat(userTaskKeys).containsSequence(userTasks.keys.asIterable())
     }
 }
